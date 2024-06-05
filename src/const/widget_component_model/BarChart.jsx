b2fc20120d534a4b1b1a29f8cf7445764e9d2119
Samrid Dangol <samrid.dango.sd@gmail.com>
2024-06-05 17:14:02 +0545

    demo

---
+++ src/const/widget_component_model/BarChart.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class BarChart extends Component {
+  render() {
+    return (
+      <div className="col-md-6">
+        <div className="card card-primary card-outline">
+          <div className="card-header">
+            <h3 className="card-title">
+              <i className="far fa-chart-bar"></i>
+              Bar Chart
+            </h3>
+            <div className="card-tools">
+              <button
+                type="button"
+                className="btn btn-tool"
+                data-card-widget="collapse"
+              >
+                <i className="fas fa-minus"></i>
+              </button>
+              <button
+                type="button"
+                className="btn btn-tool"
+                data-card-widget="remove"
+              >
+                <i className="fas fa-times"></i>
+              </button>
+            </div>
+          </div>
+          <div className="card-body">
+            <div id="bar-chart" style={{ height: "300px" }}></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BarChart;
