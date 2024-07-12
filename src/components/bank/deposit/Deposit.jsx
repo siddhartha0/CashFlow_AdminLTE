956766f23f2070dbb6196bb1fe4232d7bf374474
Luja Chitrakar <lujachitrakar156@gmail.com>
2024-07-12 11:56:10 +0545

    adding files

--- src/components/bank/deposit/Deposit.jsx
+++ src/components/bank/deposit/Deposit.jsx
@@ -1,56 +1,123 @@
-import { Component } from "react";
-import DynamicTable from "../../../const/widget_component_model/table/DynamicTable";
-import { generateRandomTransactions } from "../../../behindTheScene/api/bank";
+import { Component, useEffect, useState } from "react";
 import SingleBarChart from "../../../const/widget_component_model/charts/SingleBarChart";
 import SingleLineChart from "../../../const/widget_component_model/charts/SingleLineChart";
 import TotalView from "../TotalView";
-import ChartComponent from "../Chart";
+// import ChartComponent from "../Chart";
+import { useSelector } from "react-redux";
 import TransactionChart from "../TransactionChart";
 import DownloadButton from "../../../const/DownloadButton";
+import PropTypes from "prop-types";
+import { userbankDetails } from "../../../slices/slice/bank/UserBankSlice";
+import { useGetTransactionByUserBankIdQuery } from "../../../slices/api/transaction/TransactionApi";
 import { Link } from "react-router-dom";
+import TransactionTable from "../TransactionTable";
 
-export default class Deposit extends Component {
-  render() {
-    const headers = [
-      { key: "account", label: "Account" },
-      { key: "bank", label: "Bank" },
-      { key: "type", label: "Type" },
-      { key: "amount", label: "Amount" },
-    ];
+export default function Deposit() {
+  const userbank = useSelector(userbankDetails);
+  const [transactions, setTransactions] = useState([]);
+  const [getSelectedBank, setSelectedBank] = useState(userbank[0]);
 
-    const transaction = generateRandomTransactions(100);
-    const value = JSON.parse(localStorage.getItem("dashboard"));
-    const filterData = transaction.filter((transaction) => {
-      return transaction.status === "deposit";
+  const { data: transaction, isLoading: transactionLoading } =
+    useGetTransactionByUserBankIdQuery({ id: getSelectedBank.id });
+
+  useEffect(() => {
+    if (transaction?.entities) {
+      setTransactions(transaction?.entities);
+    }
+  }, [transaction]);
+
+  const selectBank = (bank) => {
+    setSelectedBank(bank);
+  };
+
+  const getTotalTransaction = (type) => {
+    const filteredTransactions = transactions.filter(
+      (transaction) => transaction.type === "deposits"
+    );
+    const total = filteredTransactions.reduce(
+      (sum, transaction) => sum + transaction.amount,
+      0
+    );
+    return total;
+  };
+
+  return (
+    <DepositWrapped
+      userbank={userbank}
+      selectBank={selectBank}
+      getSelectedBank={getSelectedBank}
+      getTotalTransaction={getTotalTransaction} // Pass the function as a prop
+      transactions={transactions}
+      transactionLoading={transactionLoading}
+    />
+  );
+}
+
+class DepositWrapped extends Component {
+  static propTypes = {
+    userbank: PropTypes.array,
+    selectBank: PropTypes.func,
+    getSelectedBank: PropTypes.object,
+    monthlyTransaction: PropTypes.array,
+    getTotalTransaction: PropTypes.func,
+    transactions: PropTypes.array,
+    transactionLoading: PropTypes.bool,
+  };
+
+  componentDidMount() {
+    $(function () {
+      $("#sortable").sortable();
     });
-
-    const getTotalTransaction = (status) => {
-      let total = transaction
-        .filter((transaction) => transaction.status === status)
-        .reduce((sum, transaction) => sum + transaction.amount, 0);
-
-      return total.toLocaleString();
-    };
+  }
+  render() {
+    const {
+      userbank,
+      selectBank,
+      getSelectedBank,
+      getTotalTransaction,
+      transactions,
+      transactionLoading,
+    } = this.props;
 
     return (
       <div className="container-fluid">
-        <div className="mb-3">
-          <Link to="/dashboard/bank/deposit/new" className="btn btn-primary">
-            Create Deposit
-          </Link>
-        </div>
-
         <div className="w-100">
-          <div className="d-flex justify-content-end mb-2">
-            <DownloadButton />
+          <div className="col-lg-12 d-flex justify-content-between mb-2">
+            <div>
+              <Link
+                to="/dashboard/bank/deposit/new"
+                className="btn btn-primary"
+              >
+                Create Deposit
+              </Link>
+            </div>
+            <div>
+              <DownloadButton />
+            </div>
           </div>
+
+          <div className="row mb-2 flex-nowrap">
+            {userbank.map((bank) => (
+              <div
+                className={
+                  "custom-card p-3 mr-4" +
+                  (bank === getSelectedBank ? " bg-primary" : "")
+                }
+                key={bank.bankName}
+                onClick={() => selectBank(bank)}
+              >
+                <header className="text-bold">{bank.bankName}</header>
+              </div>
+            ))}
+          </div>
+
           <div className="d-flex ">
             <div className="col-lg-4">
               <TotalView
-                data={getTotalTransaction("deposit")}
-                title="Current Balance"
-                color="info"
-                icon="fa-solid fa-money-bill-trend-up"
+                data={getSelectedBank.currentAmount}
+                title="Bank Balance"
+                color="dark"
+                icon="fa-solid fa-building-columns"
                 design="info-box"
               />
             </div>
@@ -65,7 +132,7 @@ export default class Deposit extends Component {
             </div>
             <div className="col-lg-4">
               <TotalView
-                data={filterData.length}
+                data={getTotalTransaction("transfer")}
                 title="Total Transactions"
                 color="primary"
                 icon="fa-solid fa-money-bill-trend-up"
@@ -76,11 +143,7 @@ export default class Deposit extends Component {
 
           <div className="row">
             <div className="col-lg-6">
-              <SingleBarChart
-                data={value.bankhistory}
-                label={value.label}
-                name={"Deposit"}
-              />
+              <SingleBarChart label={value.label} name={"Deposit"} />
             </div>
 
             <div className="col-lg-6">
@@ -92,15 +155,14 @@ export default class Deposit extends Component {
             </div>
             <div className="col-12 mt-4 card">
               <h2 className="text-center">Total Deposits</h2>
-              <DynamicTable
-                data={filterData}
-                headers={headers}
-                pageNo="10"
-                total={true}
+              <TransactionTable
+                transactions={transactions}
+                type="deposit"
+                title="Deposit History"
               />
             </div>
             <div className="card col-7">
-              <ChartComponent
+              {/* <ChartComponent
                 dataYearly={value.bankhistory}
                 dataWeekly={value.bankhistory}
                 dataDaily={value.bankhistory}
@@ -108,26 +170,17 @@ export default class Deposit extends Component {
                 labelWeekly={value.label}
                 labelDaily={value.label}
                 name={"Deposit"}
-              />
+              /> */}
             </div>
             <div className="col-lg-5">
               <div className="custom-card p-3">
                 <TransactionChart
-                  transactions={transaction}
+                  transactions={transactions}
                   type="deposit"
                   title="Deposit Sources"
                 />
               </div>
             </div>
-            <ChartComponent
-              dataYearly={value.bankhistory}
-              dataWeekly={value.bankhistory}
-              dataDaily={value.bankFullYearHistory[0].eachDaysAmount}
-              labelYearly={value.label}
-              labelWeekly={value.label}
-              labelDaily={value.label}
-              name={"Deposit"}
-            />
           </div>
         </div>
       </div>
